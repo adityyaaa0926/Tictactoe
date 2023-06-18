1ea9e4f056869245cc5152ae51f67fceefd0e92c
adityyaaa0926 <136965910+adityyaaa0926@users.noreply.github.com>
2023-06-18 17:07:58 +0530

    Create java.js

---
+++ java.js
@@ -0,0 +1,9 @@
+// JavaScript code for Tic Tac Toe game logic
+
+// Implement your game logic here
+
+function makeMove(row, col) {
+    // Handle the user's move
+}
+
+// Add event listeners and other necessary functions
